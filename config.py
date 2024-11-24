6e17c0aa28b46014ad7804e144d691877936c2bd
autonomus-optimus <116957912+autonomus-optimus@users.noreply.github.com>
2024-11-24 15:56:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,7 +65,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append()
+#ADMINS.append()
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
